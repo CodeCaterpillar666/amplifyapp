198e42aa206ea32cc9dcc0823037d3a230919051
ziweiwang98 <wang.ziwei2@northeastern.edu>
2022-01-28 01:41:44 -0800

    added auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifyapp9192300e": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+/**
+ * Due to update of @aws-amplify/ui-react, original tutorial doesn't work any more,
+ * this is a solution I found workable on stackoverflow
+ * https://stackoverflow.com/questions/70036160/amplifysignout-is-not-exported-from-aws-amplify-ui-react
+ */
+ import React from 'react';
+ import logo from './logo.svg';
+ import './App.css';
+ import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+ 
+ function App() {
+   return (
+     <div className="App">
+       <header>
+         <img src={logo} className="App-logo" alt="logo" />
+         <h1>We now have Auth!</h1>
+       </header>
+       <AmplifySignOut />
+     </div>
+   );
+ }
+ 
+ export default withAuthenticator(App);
